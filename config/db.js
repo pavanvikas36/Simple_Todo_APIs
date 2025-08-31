42fa2059b56e0163f9479a2989cefc17ccac124f
pavanvikas36 <pavanvivek36@gmail.com>
2025-08-31 22:11:07 +0530

    Added New Mongooes Connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mongooes = require("mongoose")
 
+const uri = "mongodb+srv://pavanvivek36_db_user:kC5Rhhone90S1AYJ@cluster0.mo0i2rv.mongodb.net/"
+// const uri = "mongodb://localhost:27017/"
+
 async function connectDB (){
     try{
-        await mongooes.connect("mongodb://localhost:27017/", {dbName: "simple_todo"})
+        await mongooes.connect(uri, {dbName: "simple_todo"})
         console.log("Database Connected Successfully")
     }catch(error){
         console.log(error)
