8548e848e2f960ee65c13df708278d92cd137ac1
pavanvikas36 <pavanvivek36@gmail.com>
2025-09-01 13:10:54 +0530

    Added New Origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded())
 
 //cors middleware
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: ["http://localhost:5173", "https://simpletodoapis.onrender.com/api/todos/getAllTodos"]
 }));
 
 //Creating APIs (Router Middleware)
