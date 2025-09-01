9e47593b1b18c4635b7e554f65cc7aa7c7017ca1
pavanvikas36 <pavanvivek36@gmail.com>
2025-09-01 13:15:42 +0530

    Adding Cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded())
 
 //cors middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "https://simpletodoapis.onrender.com/api/todos/getAllTodos"]
+    origin: ["http://localhost:5173", "https://simple-todo-react-ten.vercel.app"]
 }));
 
 //Creating APIs (Router Middleware)
